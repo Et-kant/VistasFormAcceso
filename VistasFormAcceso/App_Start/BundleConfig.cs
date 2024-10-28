cc141f0ea69a1000baa5164388c1dec1c6a375db
Emanuel <gilrestrepoemanuel@gmail.com>
2024-10-28 18:13:04 -0500

    Agregar archivos de proyecto.

---
+++ VistasFormAcceso/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace VistasFormAcceso.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/wwwroot/css").Include(
+                "~/wwwroot/lib/bootstrap/dist/css/bootstrap.min.css",
+                "~/wwwroot/css/site.css"));
+        }
+    }
+}
---
+++ VistasFormAcceso/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VistasFormAcceso.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
